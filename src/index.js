5d68c2937d1c3cb6ed78be947e854f5ab53b5688
James Hardy <jrphardy08@gmail.com>
2023-09-11 12:30:28 +0100

    Created basic interface

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, desktopCapturer, ipcRenderer } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    
+    desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
+        for (const source of sources) {
+            if (source.name === 'Electron') {
+                window.webContents.send('SET_SOURCE', source.id)
+                return
+            }
+        }
+    })
+
+    window.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
---
+++ src/peer/peer.js
@@ -0,0 +1,83 @@
+const io = require('socket.io-client');
+
+const servers = {
+    iceServers: [
+        {
+            urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+        }
+    ]
+}
+
+const constraints = {
+    audio: false,
+    video: {
+        mandatory: {
+            chromeMediaSource: 'desktop'
+        }
+    }
+}
+
+
+class Peer {
+    constructor() {
+        this.socket = io('ws://94.6.174.31:8080/');
+        this.peerConnection = new RTCPeerConnection(servers);
+
+        const socket = this.socket;
+        const peerConnection = this.peerConnection;
+
+        socket.on('peer-connect', async () => {
+            // Create stream object for receiving remote tracks
+            let localStream = await navigator.mediaDevices.getUserMedia(constraints);
+
+            localStream.getTracks().forEach((track) => {
+                peerConnection.addTrack(track, localStream);
+            })
+
+            // Create SDP offer and set local description
+            const offer = await peerConnection.createOffer();
+            await peerConnection.setLocalDescription(offer);
+
+            console.log(peerConnection)
+
+            console.log('Offer:', offer)
+            socket.emit('Offer', JSON.stringify(offer))
+        })
+
+        peerConnection.onicecandidate = (event) => {
+            console.log('New ICE Candidate:', event.candidate);
+            socket.emit('ICE Candidate', JSON.stringify(event.candidate))
+        }
+
+        socket.on('ICE Candidate', (candidate) => { peerConnection.addIceCandidate(JSON.parse(candidate)) })
+        
+        socket.on('Offer', async (data) => {
+            const remoteStream = new MediaStream();
+            document.getElementById('video-player').srcObject = remoteStream;
+
+            // Handle and assign recieved tracks to stream
+            this.peerConnection.ontrack = (event) => {
+                event.streams[0].getTracks().forEach((track) => {
+                    remoteStream.addTrack(track)
+                })
+            }
+
+            const offer = JSON.parse(data)
+            await peerConnection.setRemoteDescription(offer);
+            
+            const answer = await peerConnection.createAnswer();
+            console.log('Answer:', answer)
+            await peerConnection.setLocalDescription(answer);
+            socket.emit('Answer', JSON.stringify(answer))
+        })
+
+        socket.on('Answer', (answer) => peerConnection.setRemoteDescription(JSON.parse(answer)))
+    }
+
+    async connect() {
+        this.socket.emit('peer-connect');
+    }
+    
+}
+
+module.exports = Peer;
