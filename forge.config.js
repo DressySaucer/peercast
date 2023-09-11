2b15630cd2fdd8ba7d0f7649491b048eeb3114d1
James Hardy <134983126+DressySaucer@users.noreply.github.com>
2023-09-11 21:47:47 +0100

    Create build automation

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,14 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
-  rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
       name: '@electron-forge/maker-zip',
+      platforms: ["win32"],
       config: {},
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: '@electron-forge/maker-dmg',
+      platforms: ["darwin"],
       config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+    }
+  ]
 };
